40da09197017478a8db9052b21114343c0556382
GuoHyan <641420731@qq.com>
2019-09-02 17:36:56 +0800

    myVue_V1.0版本

---
+++ my_vue/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="HelloWorld">
+    <input type="text" placeholder="请输入文字" v-model="msg">
+    <p>{{msg}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '大家好!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
